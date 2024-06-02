4a7e4065f85f7d2869623b50eb70b77f3a0a07cf
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-06-01 20:24:19 -0400

    fix: path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), tsconfigPaths()],
+    base: '/',
+    test: {
+      globals: true,
+      environment: 'happy-dom',
+      setupFiles: '.vitest/setup',
+      include: ['**/test.{ts,tsx}']
+    }
   }
+
+  if (command !== 'serve') {
+    config.base = '/release-please-test /'
+  }
+
+  return config
 })
