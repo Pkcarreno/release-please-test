246937481c7a7d7e4059c52ebd83c69cad1ae568
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-06-02 02:03:09 -0400

    feat: test env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,25 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
-export default defineConfig(({ command }) => {
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  process.env.BASE_URL = process.env.VITE_BASE_URL ?? '/'
   const config = {
     plugins: [react(), tsconfigPaths()],
-    base: '/',
+    base: process.env.BASE_URL,
+    define: {
+      APP_VERSION: JSON.stringify(process.env.npm_package_version),
+      BASE_URL: JSON.stringify(process.env.BASE_URL)
+    },
     test: {
       globals: true,
       environment: 'happy-dom',
       setupFiles: '.vitest/setup',
       include: ['**/test.{ts,tsx}']
     }
-  }
-
-  if (command !== 'serve') {
-    config.base = '/release-please-test/'
   }
 
   return config
